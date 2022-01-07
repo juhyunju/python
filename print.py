cb758d3d5005b58fe694437e4d925fc14e53cb34
juhyunju <juhyunju@naver.com>
2022-01-07 14:39:31 +0900

    str.format 함수 사용

--- print.py
+++ print.py
@@ -3,3 +3,9 @@ print('안녕하세요 주현입니다.')
 print(78)
 
 print(type(1.23456))
+
+print("{}".format(78))
+print("{} {:x}".format(78,78))
+print("{:.0f}".format(1,23456))
+print("{:.2f}".format(1,23456))
+print("{:.4f}".format(1,23456))
