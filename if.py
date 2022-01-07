d3906cf0df1868412904faa80c26c6f5b7fd48c0
juhyunju <juhyunju@naver.com>
2022-01-07 14:48:32 +0900

    else if 시용

--- if.py
+++ if.py
@@ -1,3 +1,11 @@
-x = 3
-if x == 3: # x가 3이면 if 문 이하 실행
-    print("x==3")
+# x = 3
+# if x == 3: # x가 3이면 if 문 이하 실행
+#     print("x==3")
+
+x = 1
+if x == 3 :
+    print("x == 3")
+elif x ==4:
+    print("x == 4")
+else:
+    print("둘다아님")
