3692942ec1a985b58564ae7727b529aa27843016
juhyunju <juhyunju@naver.com>
2022-01-07 14:57:46 +0900

    range 활용

--- for.py
+++ for.py
@@ -1,6 +1,13 @@
-sum = 0
-for i in range(11):
-    sum = sum+i
+# sum = 0
+# for i in range(11):
+#     sum = sum+i
 
-print("i=",i)
-print("sum=",sum)
+# print("i=",i)
+# print("sum=",sum)
+
+sum = 0
+for i in range(1,11,2):
+    sum = sum + i
+    print("i = ",i)
+
+print("sum =",sum)
