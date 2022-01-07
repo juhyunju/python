d9f056da67c5b7c73a6055fec2227b30e1f4888c
juhyunju <juhyunju@naver.com>
2022-01-07 15:00:17 +0900

    while 활용

--- while.py
+++ while.py
@@ -1,6 +1,14 @@
-try:
-    while True:
-        print("Hello~")
+# try:
+#     while True:
+#         print("Hello~")
 
-except KeyboardInterrupt:
-    pass
+# except KeyboardInterrupt:
+#     pass
+
+sum = 0
+i = 1
+while i<11:
+    sum = sum + i
+    print("i=",i)
+    i = i+1
+print("sum = ",sum)
